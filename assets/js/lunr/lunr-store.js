238667e0b930b8a3d4249898b15efe3039e35c9d
HappyCerberus <HappyCerberus@users.noreply.github.com>
2024-05-10 09:06:16 +0000

    jekyll build from Action 0e65f116fe3cedf62effa0b98d64152536a17ff7

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "C++20 Modules - Complete Guide",
+        "excerpt":"One of the four major features of C++20, modules bring this common feature from other languages into C++.   With this, we finally get idempotent compilation and massive speedups resulting from the removal of recursive include expansion.      Article    Video    PDF Download    ","categories": [],
+        "tags": [],
+        "url": "/cpp20_guides/cpp20_modules.html",
+        "teaser": null
+      },{
+        "title": "C++20 Concepts - Complete Guide",
+        "excerpt":"Concepts bring language-level support for limiting which types can participate in template specialisation without relying on language side effects through SFINAE.   When using concepts, templates and auto can be easily constrained, which allows for better compiler diagnostic messages and complex specialisation selection.      Article    Video    PDF Download    ","categories": [],
+        "tags": [],
+        "url": "/cpp20_guides/cpp20_concepts.html",
+        "teaser": null
+      },{
+        "title": "C++20 Coroutines - Complete* Guide",
+        "excerpt":"C++20 brought us initial support for building coroutines.   This support is mainly aimed at library implementors. However, the main building blocks are all in place.      Article    Video     PDF Download    ","categories": [],
+        "tags": [],
+        "url": "/cpp20_guides/cpp20_coroutines.html",
+        "teaser": null
+      },{
+        "title": "C++20 Guides",
+        "excerpt":"A series of C++20 guides covering the main features. C++20 Modules - Complete Guide One of the four major features of C++20, modules bring this common feature from other languages into C++. With this, we finally get idempotent compilation and massive speedups resulting from the removal of recursive include expansion....","categories": [],
+        "tags": [],
+        "url": "/guides/cpp20_guides/",
+        "teaser": null
+      },]
